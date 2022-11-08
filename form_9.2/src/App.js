c3e97c210e148e008559eda395a28da86dab3694
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2022-11-08 19:54:38 +0300

    commit 08.11.22

--- form_9.2/src/App.js
+++ form_9.2/src/App.js
@@ -1,12 +1,21 @@
 // import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import EditProfileForm from "./components/editProfileForm";
+import ProfilePage from "./components/profilePage";
 
 function App() {
+    const [edit, setEdit] = !!localStorage.getItem("user");
+
+    const handlerEditOn = () => setEdit(true);
+    const handlerEditOn = () => setEdit(true);
+
     return (
         <div className="App">
             <div>
-                <h1 className="text-3xl font-bold underline">Hello World</h1>
+                <h1 className="text-3xl font-bold underline">Use profiler: </h1>
             </div>
+            {edit ? <EditProfileForm /> : <ProfilePage />}
         </div>
     );
 }
