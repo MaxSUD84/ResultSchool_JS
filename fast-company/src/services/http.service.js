98673f3e8fc76eaf1b3ca0f4442362ef7d641ca2
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2022-11-29 15:50:17 +0300

    lesson 15 done

--- fast-company/src/services/http.service.js
+++ fast-company/src/services/http.service.js
@@ -1,13 +1,40 @@
 import axios from "axios";
+import { func } from "prop-types";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import config from "../config.json";
+import configFile from "../config.json";
 
 // использовать только для обращения к статическому адресу BackEnd Server'а
-axios.defaults.baseURL = config.apiEndpoint;
+axios.defaults.baseURL = configFile.apiEndpoint;
+
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.isFireBase) {
+            const containSlash = /\/$/gi.test(config.url);
+            config.url =
+                (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
+            // console.log(config.url);
+        }
+
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+function transformData(data) {
+    return data ? Object.keys(data).map((key) => ({ ...data[key] })) : [];
+}
 
 axios.interceptors.response.use(
-    (res) => res,
+    (res) => {
+        if (configFile.isFireBase) {
+            res.data = { content: transformData(res.data) };
+            // console.log(res.data);
+        }
+        return res;
+    },
     function (error) {
         const expectedErrors =
             error.response &&
