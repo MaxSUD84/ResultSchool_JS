3a5e0683b4001edf38a509219a96d51de1db018f
MaxG <113623110+MaxSUD84@users.noreply.github.com>
2023-01-11 00:10:23 +0300

    12 lesson done

--- rrd-demo-project/src/App.jsx
+++ rrd-demo-project/src/App.jsx
@@ -1,45 +1,58 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import AuthLayout from "./layouts/AuthLayout";
-import PostsLayout from "./layouts/PostsLayout";
-import MainPage from "./pages/MainPage";
 import NavBar from "./components/NavBar/NavBar";
 // import ProtectedRoute from "./components/ProtectedRoute";
 import withRedux from "./hoc/withRedux";
 import withRouter from "./hoc/withRouter";
 import "react-toastify/dist/ReactToastify.css";
-
-// Pages
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SigupPage";
-import PostsListPage from "./pages/Posts/PostsListPage";
-import PostPage from "./pages/Posts/PostPage";
+// Routes
+import routes from "./routes";
+import { useSelector } from "react-redux";
+import { isLoggedInSelector } from "./store/authSlice";
 
 function App() {
-    return (
-        <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
-            <NavBar />
-            <Routes>
-                <Route index element={<MainPage />} />
-                <Route path="auth/*" element={<AuthLayout />}>
-                    <Route index element={<Navigate to="/auth/signup" />} />
-                    <Route path={"login"} element={<LoginPage />} />
-                    <Route path={"signup"} element={<SignUpPage />} />
-                    <Route path="*" element={<Navigate to="/auth/signup" />} />
-                </Route>
-                <Route path="posts/*" element={<PostsLayout />}>
-                    <Route index element={<PostsListPage />} />
-                    {/* редирект при ошибочном параметре пути реализован внутри PostPage */}
-                    <Route path=":postId" element={<PostPage />} />
-                </Route>
-                <Route path="*" element={<Navigate to="/posts" />} />
-                {/* <ProtectedRoute path="/posts/:id?" element={<PostsLayout />} /> */}
-            </Routes>
+  const isLoggedIn = useSelector(isLoggedInSelector());
+  const location = useLocation();
+  const element = useRoutes(routes(isLoggedIn, location));
+  return (
+    <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
+      <NavBar />
+      {element}
+      <ToastContainer />
+    </div>
+  );
 
-            <ToastContainer />
-        </div>
-    );
+  //   return (
+  //     <div className="min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col">
+  //       <NavBar />
+  //       <Routes>
+  //         <Route index element={<MainPage />} />
+  //         <Route path="auth/*" element={<AuthLayout />}>
+  //           <Route index element={<Navigate to="/auth/signup" />} />
+  //           <Route path={"login"} element={<LoginPage />} />
+  //           <Route path={"signup"} element={<SignUpPage />} />
+  //           <Route path="*" element={<Navigate to="/auth/signup" />} />
+  //         </Route>
+  //         <Route
+  //           path="posts"
+  //           element={
+  //             <ProtectedRoute redirectTo={"/auth/login"}>
+  //               <PostsLayout />
+  //             </ProtectedRoute>
+  //           }
+  //         >
+  //           {/* <Route path="posts/*" element={<PostsLayout />}> */}
+  //           <Route index element={<PostsListPage />} />
+  //           {/* редирект при ошибочном параметре пути реализован внутри PostPage */}
+  //           <Route path=":postId" element={<PostPage />} />
+  //         </Route>
+  //         <Route path="*" element={<Navigate to="/posts" />} />
+  //       </Routes>
+
+  //       <ToastContainer />
+  //     </div>
+  //   );
 }
 const AppWithStoreAndRoutes = withRedux(withRouter(App));
 export default AppWithStoreAndRoutes;
---
+++ rrd-demo-project/src/routes.jsx
@@ -0,0 +1,70 @@
+import { Navigate } from "react-router-dom";
+
+import AuthLayout from "./layouts/AuthLayout";
+import PostsLayout from "./layouts/PostsLayout";
+import MainPage from "./pages/MainPage";
+
+// Pages
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SigupPage";
+import PostsListPage from "./pages/Posts/PostsListPage";
+import PostPage from "./pages/Posts/PostPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const routes = (isLoggedIn, location) => [
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="/auth/signup" />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/auth/signup" />,
+      },
+    ],
+  },
+  {
+    path: "posts",
+    element: isLoggedIn ? (
+      <PostsLayout />
+    ) : (
+      <Navigate to="/auth/login" state={{ referrer: location }} />
+    ),
+    // element: (
+    //   <ProtectedRoute redirectTo={"/auth/login"}>
+    //     <PostsLayout />
+    //   </ProtectedRoute>
+    // ),
+    children: [
+      {
+        path: "",
+        element: <PostsListPage />,
+      },
+      {
+        path: ":postId",
+        element: <PostPage />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to={isLoggedIn ? "/posts" : "/"} />,
+  },
+];
+
+export default routes;
